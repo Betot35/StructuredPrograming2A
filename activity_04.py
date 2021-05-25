9f19a92a73372871a1cb4bdd5c20656b68e51603
Betot35 <84750692+Betot35@users.noreply.github.com>
2021-05-25 00:05:21 -0500

    Add files via upload

---
+++ activity_04.py
@@ -0,0 +1,12 @@
+print("START")
+num1= 0
+num2= 0
+
+num1= int(input("Enter number 1"))
+num2= int(input("Enter number 2"))
+result_add= num1+num2
+result_m= num1*num2
+
+print("This is the add:",result_add)
+print("This is the multiplication:",result_m)
+print("END")
---
+++ activity_06.py
@@ -0,0 +1,9 @@
+print("Start")
+counter=1
+add=0
+if (counter<=100):
+    add= add+counter
+    counter= counter+1
+else:
+    print("The answer is ",add)
+print("End")
