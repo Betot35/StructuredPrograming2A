146bd3c02ca01b2cf509c8463bb532f042d6469e
Betot35 <84750692+Betot35@users.noreply.github.com>
2021-08-13 15:58:04 -0500

    Add files via upload

---
+++ Unit2extempo/arg_mainc.c
@@ -0,0 +1,12 @@
+#include <studio.h>
+
+int my_int -a; // %i o %d
+float my_float =0; // %f
+char my_char = 'c'; //%c
+char my_str[] = "Hola"; //%s
+
+int main( int argc, char** argv){
+
+     printf( "argc: #i, element 1: %s, elem 2 : %s char: %c \n", argc, argv[1], argv[2],'L' );
+     return 0;
+}
